26adba93af18af5acaf96d262350ba2c793965f5
Gauthier Hellot <ghe@f4-group.com>
2016-09-01 13:46:54 +0200

    site

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        stylus: {
+            compile: {
+                options: {
+                    paths: ['stylus'],
+                },
+                files: {
+                    'css/style.css': 'stylus/style.styl',
+                },
+            },
+
+        },
+
+        pug: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    'index.html': 'pug/index.pug',
+                }
+            }
+        },
+
+        watch: {
+            stylus: {
+                files: ['stylus/*.styl'],
+                tasks: ['stylus:compile'],   // This needs to be "tasks" (not "task")
+                options: {livereload: true},
+            },
+            pug: {
+                files: ['pug/*.pug'],
+                tasks: ['pug:compile'],   // This needs to be "tasks" (not "task")
+                options: {livereload: true},
+            },
+        },
+
+
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.registerTask('compile', ['newer:stylus:all']);
+};
